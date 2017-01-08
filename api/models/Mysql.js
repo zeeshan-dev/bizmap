5f0dce422042b9d2d3d48e97f135e88e7949245c
Muhammad <muhammad.zeeshan@riksof.com>
2017-01-08 19:48:10 +0500

    renamed file

---
+++ api/models/Mysql.js
@@ -0,0 +1,112 @@
+// Constructor
+function MySQL() {
+  
+}
+
+// prepare insert query
+MySQL.prototype.perpareInsertQuery = function(table, data) {
+  
+  var query = 'INSERT INTO ' + table + ' ';
+
+  var columns = '(';
+  var values = '(';
+  
+  // prepare data 
+  for (var key in data) {
+  
+    columns += key + ',';
+    values += "'" + data[key] + "',";
+  }
+
+  columns = columns.replace(/,\s*$/, "") + ')';
+  values = values.replace(/,\s*$/, "") + ')'
+  query = query + columns + ' VALUES ' + values;
+  
+  return query;
+};
+
+// prepare select query
+MySQL.prototype.perpareSelectQuery = function(table, columns, start, limit) {
+  
+  var query = '';
+  
+  if ( columns.length === 0 ) {
+    query = 'SELECT * FROM ' + table;
+  } else {
+
+    var fetchColumns = '';
+    // prepare data 
+    for (var column in columns) {
+      fetchColumns += column + ',';
+    }
+
+    fetchColumns = fetchColumns.replace(/,\s*$/, "");
+
+    query = 'SELECT '+ fetchColumns +' FROM ' + table;
+  }
+
+  if ( start && start > 0 && limit && limit > 0 ) {
+    query = query + ' LIMIT ' + start + ',' + limit;
+  }
+
+  return query;
+};
+
+/**
+* 
+* @params
+* 1- name, business name (String)
+* 2- location, location code (Number)
+* 3- category, category code (Number)
+* 4- start, query start limit (Number)
+* 5- count, is count query (Boolean)
+**/
+MySQL.prototype.perpareSearchQuery = function(name, location, category, start, count) {
+ 
+  var query = ' SELECT ';
+  var columns = ' b.name, b.address, b.phone1, b.phone2, c.name as cityName ';
+
+  if ( count ) {
+    columns = ' COUNT(*) as total'
+  }
+  
+  var from = ' FROM business as b ';
+
+  query += columns + from + ' LEFT JOIN city as c ON c.code = b.cityCode ';
+
+  var where = ' WHERE ';
+
+  // search on name field
+  if (name !== '') {
+    where += "b.name REGEXP '" + name + "'";
+  }
+
+  if ( location !== '' ) {
+
+    var and = (name == '') ? ' ' : ' AND '; 
+    where += and + 'c.code = ' + location;
+  }
+
+  query = query + where + ' LIMIT ' + start + ',' + 20 + ';';
+  console.log(query);
+  return query;
+};
+
+/**
+* 
+* @params
+* 1- db  mysql db object
+* 2- query String
+* 3- callback function
+**/
+MySQL.prototype.executeQuery = function(db, query, callback) {
+
+  db.query( query, function queryCallback(err, result) {
+    callback(err, result);
+  });
+
+};
+
+
+// export the class
+module.exports = MySQL;
